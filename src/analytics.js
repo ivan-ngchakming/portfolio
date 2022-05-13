86e57b2a27d9bd2912ea540f71bbb2164a298de9
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-05-13 18:58:20 +0800

    format all code

--- src/analytics.js
+++ src/analytics.js
@@ -1,24 +1,26 @@
 // Global site tag (gtag.js) - Google Analytics
 
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
 
-gtag('config', 'G-MVTZNR8GXZ');
+gtag("config", "G-MVTZNR8GXZ");
 
 function track(project, mode) {
-  gtag('event', 'view_project', {
+  gtag("event", "view_project", {
     project: project,
     mode: mode,
-  })
+  });
 }
 
 function trackSocial(social) {
-  gtag('event', 'view_social', {
+  gtag("event", "view_social", {
     social: social,
-  })
+  });
 }
 
 function trackResumeDownload() {
-  gtag('event', 'download_resume', {})
+  gtag("event", "download_resume", {});
 }
