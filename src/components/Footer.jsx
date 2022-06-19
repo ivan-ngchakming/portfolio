905dc256ee23c8e7b748969e85cc24f32aa311e5
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-06-19 18:38:32 +0800

    implemented footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
+import cn from "classnames";
+
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>Footer</p>
+    <footer
+      className={cn(
+        "p-4",
+        "pb-8",
+        "flex",
+        "justify-center",
+        "snap-end",
+        "text-slate-400"
+      )}
+    >
+      <div>
+        <a
+          href="https://github.com/ivan-ngchakming/portfolio"
+          rel="noopener noreferrer"
+          target="_blank"
+          className={cn("flex", "flex-col", "items-center", "justify-center")}
+        >
+          <div>Built by Ivan Ng</div>
+          <div>@ {new Date().getFullYear()}</div>
+        </a>
+      </div>
     </footer>
   );
 };
