a3726605ea414441dfea43070750d3fd0cc03c8c
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-06-19 18:31:14 +0800

    add about me content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,45 +1,124 @@
 import cn from "classnames";
 import PageContainer from "./common/PageContainer";
 
+const Paragraph = ({ children }) => (
+  <p
+    className={cn(
+      "font-sans",
+      "text-base",
+      "font-medium",
+      "text-slate-400",
+      "mb-6"
+    )}
+  >
+    {children}
+  </p>
+);
+
 const About = () => {
   return (
-    <PageContainer id="about" className={cn("flex", "flex-col")}>
-      <div className={cn("flex", "items-center", "mb-10")}>
+    <PageContainer
+      id="about"
+      className={cn("flex", "flex-col", "items-center")}
+    >
+      <div
+        className={cn(
+          "flex",
+          "items-center",
+          "justify-start",
+          "w-full",
+          "mb-10",
+          "sm:px-12"
+        )}
+      >
         <h2
-          className={cn("text-2xl", "text-slate-400", "font-sans", "font-bold")}
+          className={cn(
+            "text-2xl",
+            "text-slate-400",
+            "font-sans",
+            "font-bold",
+            "min-w-fit"
+          )}
         >
-          <span className={cn("text-brightred", "font-mono")}>01.</span> About
-          Me{" "}
+          <span className={cn("text-brightred", "font-mono", "text-base")}>
+            01.
+          </span>{" "}
+          About Me{" "}
         </h2>
         <span
           className={cn(
             "border-b-[0.1px]",
             "border-slate-600",
             "h-[1px]",
-            "w-40",
+            // "min-w-full",
+            "grow",
+            "sm:w-40",
             "ml-8"
           )}
         ></span>
       </div>
-      <div>
-        <p>
-          Hello! My name is Brittany and I enjoy creating things that live on
-          the internet. My interest in web development started back in 2012 when
-          I decided to try editing custom Tumblr themes — turns out hacking
-          together a custom reblog button taught me a lot about HTML & CSS!
-        </p>
-        <p>
-          Fast-forward to today, and I’ve had the privilege of working at an
-          advertising agency, a start-up, a huge corporation, and a student-led
-          design studio. My main focus these days is building accessible,
-          inclusive products and digital experiences at Upstatement for a
-          variety of clients.
-        </p>
-        <p>
-          I also recently launched a course that covers everything you need to
-          build a web app with the Spotify API using Node & React.
-        </p>
-        <p>Here are a few technologies I’ve been working with recently:</p>
+      <div className="sm:flex">
+        <div className={cn("sm:mr-8", "mb-8", "sm:mb-0", "sm:max-w-lg")}>
+          <Paragraph>
+            Hello! My name is Ivan and I enjoy creating things that live on the
+            internet. My interest in web development started when I had to hack
+            together a website for my Club in University. Since then, I have
+            worked on various projects and have been learning new web technology
+            every day.
+          </Paragraph>
+          <Paragraph>
+            Here are a few technologies I’ve been working with recently:
+          </Paragraph>
+          <div className={cn("flex", "text-slate-400")}>
+            <ul
+              className={cn(
+                "list-disc",
+                "marker:text-brightred",
+                "marker:content-['▹']",
+                "marker:tracking-[0.5em]",
+                "pl-6",
+                "mr-10"
+              )}
+            >
+              <li>JavaScript (ES6+)</li>
+              <li>React</li>
+              <li>Node.js</li>
+            </ul>
+            <ul
+              className={cn(
+                "list-disc",
+                "pl-6",
+                "marker:text-brightred",
+                "marker:content-['▹']",
+                "marker:tracking-[0.5em]"
+              )}
+            >
+              <li>Typescript</li>
+              <li>Python</li>
+              <li>Golang</li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex justify-center items-center relative">
+          <img
+            src="/me.jpg"
+            className="max-w-xs w-72 h-72 object-cover rounded-md"
+          />
+          <div
+            className={cn(
+              "absolute",
+              "w-72",
+              "h-72",
+              "border-2",
+              "border-brightred",
+              "transition-transform",
+              "translate-x-4",
+              "translate-y-4",
+              "-z-10",
+              "rounded-md"
+            )}
+          ></div>
+        </div>
       </div>
     </PageContainer>
   );
